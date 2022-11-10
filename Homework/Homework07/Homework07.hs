1595a15d5d2def332cb0c9d9e3460342a129baff
sergi <s@s.com>
2022-11-10 08:17:41 +0100

    Homeworks 07

--- Homework/Homework07/Homework07.hs
+++ Homework/Homework07/Homework07.hs
@@ -1,6 +1,8 @@
 -- Question 1
 -- Investigate the `Bounded` type class. What behaviours it provides?
 
+-- min and max values for a type
+
 -- The Bounded type class class is used to name the upper and lower limits of a type.
 -- It provides the minBound and maxBound behaviours to check the minimum and maximum values of a type.
 
@@ -20,6 +22,9 @@
 
 -- Question 3
 -- Investigate the `Enum` type class. What behaviours provides?
+
+-- Expecting one more argument to ‘Enum’
+-- Expected a type, but ‘Enum’ has kind ‘* -> Constraint’
 
 -- The Enum class defines operations on sequentially ordered types.
 -- Two important behaviors of this class are:
@@ -43,8 +48,25 @@ f2 x = if x == maxBound then minBound else succ x
 -- it doesn't know which numeric type you're passing and not all numeric types are instances of Bounded.
 -- You could solve this by specifying that the number is an Int or Word, for example.
 
+-- f2 4 :: Int
+
 -- Question 5
 -- Investigate the numeric type classes to figure out which behaviors they provide to change between numeric types.
 
 -- We could use and combine several behaviors: `fromInteger`, `toInteger`, and `fromRational`. Although other
 -- type classes we didn't cover provide `round` `ceiling`, etc. that we can also use to go from fractions to Integrals.
+
+-- https://wiki.haskell.org/Converting_numbers
+{-
+-- fromInteger :: Num a => Integer -> a
+f3 :: (Num a) => a -> a
+f3 x = x * fromInteger 0.5
+-- toInteger:: Integral a => a -> Integer
+f4 :: (Integral a) =>  a -> a
+f4 x = x *  0.5
+-}
+-- fromRational :: Fractional a => Rational -> a
+f5 :: (Fractional a) => a -> a
+f5 x = x * 0.5
+
+--log (fromIntegral a)
